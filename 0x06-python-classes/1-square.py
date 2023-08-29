e1787aecab3431a4975cbddede5a76a5604e7513
LAKRIM <lakrimoussama@gmail.com>
2023-08-29 21:54:07 +0100

    Task1: class Square defines a square by size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defiune a class named Square"""
+
+class Square:
+    """represent a square"""
+
+    def __int__(self, size):
+        """Initializing a new square.
+
+        Args:
+            size (int): The size of the new square
+        """
+        self.__size = size
